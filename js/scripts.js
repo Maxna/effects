60d8adecf1a526728c443e6f65d7123acb014615
Matt Dowdney and Matt Nardoni <matt@>
2018-03-20 09:58:02 -0700

    add content, images, css and js formatting

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#clickable").click(function() {
+    $("#initially-hidden").toggle();
+    $("#initially-showing").toggle();
+  });
+  $("#clickable2").click(function() {
+    $("#initially-hidden2").toggle();
+    $("#initially-showing2").toggle();
+  });
+  $("#clickable3").click(function() {
+    $("#initially-hidden3").toggle();
+    $("#initially-showing3").toggle();
+  });
+});
