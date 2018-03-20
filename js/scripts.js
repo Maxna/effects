36213b7bac4e7aed006be881e22a8f088f73de8a
Matt Dowdney and Matt Nardoni <matt@>
2018-03-20 10:04:20 -0700

    fix click behavior

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  $("#clickable").click(function() {
+  $(".clickable").click(function() {
     $("#initially-hidden").toggle();
     $("#initially-showing").toggle();
   });
-  $("#clickable2").click(function() {
+  $(".clickable2").click(function() {
     $("#initially-hidden2").toggle();
     $("#initially-showing2").toggle();
   });
-  $("#clickable3").click(function() {
+  $(".clickable3").click(function() {
     $("#initially-hidden3").toggle();
     $("#initially-showing3").toggle();
   });
